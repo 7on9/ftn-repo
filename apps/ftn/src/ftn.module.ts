05896e3635e2ae0adcfccb16b9f78a833670d378
Leo Nguyen <devleo@gbg-group.net>
2025-05-15 17:57:40 +0700

    Remove unused FtnController, FtnService, and related tests; refactor FtnModule to integrate global modules and improve structure

--- apps/ftn/src/ftn.module.ts
+++ apps/ftn/src/ftn.module.ts
@@ -1,10 +1,59 @@
-import { Module } from '@nestjs/common';
-import { FtnController } from './ftn.controller';
-import { FtnService } from './ftn.service';
+import { DynamicModule, Module } from '@nestjs/common';
+import * as allModules from './x-modules';
+import { JwtModule } from '@nestjs/jwt';
+import { configEnv } from '~/libs/@config/env';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CacheModule } from '@nestjs/cache-manager';
+
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import { join } from 'path';
+import { TechUtils } from '~/libs/@core/utils';
+
+const envConfig = configEnv();
+const { JWT_EXPIRY, JWT_SECRET } = envConfig;
+
+const multipleDatabaseModule: DynamicModule[] = TechUtils.dbModules(
+  envConfig.DBS,
+);
+
+const globalModules = [
+  JwtModule.register({
+    global: true,
+    secret: JWT_SECRET,
+    signOptions: { expiresIn: JWT_EXPIRY, algorithm: 'HS256' },
+  }),
+  ScheduleModule.forRoot(),
+  CacheModule.register({
+    isGlobal: true,
+    store: 'memory',
+  }),
+  I18nModule.forRootAsync({
+    useFactory: () => ({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: join(__dirname, '/libs/assets/locales/'),
+        watch: true,
+      },
+      typesOutputPath: join(__dirname, '../../libs/assets/i18n.generated.ts'),
+    }),
+    resolvers: [
+      { use: QueryResolver, options: ['lang'] },
+      AcceptLanguageResolver,
+      new HeaderResolver(['x-lang']),
+    ],
+  }),
+];
 
 @Module({
-  imports: [],
-  controllers: [FtnController],
-  providers: [FtnService],
+  imports: [
+    ...globalModules,
+    ...Object.values(allModules),
+    ...multipleDatabaseModule,
+  ],
 })
 export class FtnModule {}
--- apps/ftn/src/main.ts
+++ apps/ftn/src/main.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { FtnModule } from './ftn.module';
 import timezone from 'dayjs/plugin/timezone';
@@ -6,17 +10,107 @@ import dayjs from 'dayjs';
 import weekday from 'dayjs/plugin/weekday';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 
-
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { setupTransactionContext } from '~/libs/@core/decorator';
+import { GlobalPrefix } from '~/libs/common/constants';
+import { ValidatePipe } from '~/libs/@systems/pipe';
+import { I18nValidationExceptionFilter } from 'nestjs-i18n';
+import {
+  HttpExceptionFilter,
+  TypeOrmFilter,
+} from '~/libs/@systems/exceptions/http-exception-filter';
+import { configEnv } from '~/libs/@config/env';
+import { INestApplication } from '@nestjs/common';
+import { getFromContainer, MetadataStorage } from 'class-validator';
+import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
+import { SchemasObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { ApiException } from '~/libs/@systems/exceptions';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(weekday);
 dayjs.extend(isSameOrAfter);
 
+const configSwagger = (app: INestApplication) => {
+  const { SWAGGER_TITLE, SWAGGER_DESCRIPTION, SWAGGER_VERSION } = configEnv();
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER_TITLE)
+    .setDescription(SWAGGER_DESCRIPTION)
+    .setVersion(SWAGGER_VERSION)
+    .addSecurity('bearer', {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options, {
+    // extraModels: [PageResponse]
+  });
+
+  // Creating all the swagger schemas based on the class-validator decorators
+  const metadatas = (getFromContainer(MetadataStorage) as any)
+    .validationMetadatas;
+  const targetSchemas = document?.components?.schemas || {};
+  const schemasBinding = validationMetadatasToSchemas(metadatas) || {};
+
+  Object.keys(schemasBinding).forEach((key) => {
+    const value = schemasBinding[key] as SchemasObject;
+    if (!targetSchemas[key]) {
+      Object.assign(targetSchemas, {
+        key: value,
+      });
+    } else {
+      const targetValue = targetSchemas[key] as SchemasObject;
+
+      Object.assign(targetValue.properties, value.properties);
+      targetValue.required = value.required;
+      Object.assign(targetSchemas, {
+        key: targetValue,
+      });
+    }
+  });
+  if (document.components?.schemas) {
+    document.components.schemas = Object.assign({}, targetSchemas);
+  }
+  SwaggerModule.setup('swagger', app, document);
+};
+
 async function bootstrap() {
+  const { REQUEST_TIMEOUT = 30 * 60 * 1000 } = configEnv();
+  setupTransactionContext();
   const app = await NestFactory.create(FtnModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  //#region Common config
+  app.enableCors({
+    origin: '*',
+  });
+  app.setGlobalPrefix(GlobalPrefix.API);
+  app.useGlobalPipes(
+    new ValidatePipe({
+      whitelist: false, // Chỉ giữ lại các field có trong DTO
+      forbidNonWhitelisted: false, // Chặn field không có trong DTO
+      transform: true, // Chuyển đổi kiểu dữ liệu tự động
+    }),
+  );
+  app.useGlobalFilters(new I18nValidationExceptionFilter());
+  // app.useGlobalInterceptors(new TransformInterceptor())
+  app.useGlobalFilters(new HttpExceptionFilter(), new TypeOrmFilter());
+  app.use(
+    (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
+      const { REQUEST_TIMEOUT = 30 * 60 * 1000 } = configEnv();
+      res.setTimeout(REQUEST_TIMEOUT, () => {
+        next(new ApiException('REQUEST TIMEOUT'));
+      });
+      next();
+    },
+  );
+
+  //#endregion
+  configSwagger(app);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
